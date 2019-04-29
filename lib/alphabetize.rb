c8f90f8dabcd18ad6958f7b0f48135c86cfcc63d
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-29 20:52:07 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,8 @@
-ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 
 def alphabetize(arr)
-  
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.chars.map { |letter| ESPERANTO_ALPHABET.index(letter)}
+  end
 end
